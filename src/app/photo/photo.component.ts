f338a786a8f556160ff2bef44bc6109dfdbc784b
Kyzfome2 <domka_201811@gmail.com>
2024-09-22 21:01:25 +0300

    fixed-bugs

--- src/app/photo/photo.component.ts
+++ src/app/photo/photo.component.ts
@@ -9,9 +9,7 @@ export class PhotoComponent {
   images = [
     { name: 'Арка', url: 'assets/images/arka.jpg' },
     { name: 'Artwork', url: 'assets/images/Artwork.png' },
-    { name: 'City', url: 'assets/images/city.png' },
     { name: 'Do', url: 'assets/images/do.png' },
-    { name: 'Forest', url: 'assets/images/forest.png' },
     {
       name: 'Hotel France Vinnystia Exterior',
       url: 'assets/images/Hotel-France-Vinnytsia-Exterior.jpg',
@@ -20,9 +18,9 @@ export class PhotoComponent {
     { name: 'Logo', url: 'assets/images/logo.png' },
     { name: 'MA Office', url: 'assets/images/ma-office.png' },
     { name: 'Man', url: 'assets/images/man.png' },
-    { name: 'Maxresdefault', url: 'assets/images/maxresdefault.jpg' },
-    { name: 'Myr 1', url: 'assets/images/myr_1.png' },
-    { name: 'Myr 2', url: 'assets/images/myr_2.png' },
+    { name: 'Собор', url: 'assets/images/maxresdefault.jpg' },
+    { name: 'Мур 1', url: 'assets/images/myr_1.png' },
+    { name: 'Мур 2', url: 'assets/images/myr_2.png' },
     { name: 'Names', url: 'assets/images/names.png' },
     { name: 'Photo', url: 'assets/images/photo.png' },
     { name: 'Rada', url: 'assets/images/rada.png' },
@@ -32,6 +30,6 @@ export class PhotoComponent {
     { name: 'Tram', url: 'assets/images/tram.png' },
     { name: 'Tram 1', url: 'assets/images/tram1.png' },
     { name: 'Vinnytsia Oblrada', url: 'assets/images/vinnytsia_oblrada.jpg' },
-    { name: 'Муры 1', url: 'assets/images/мури 1.png' },
+    { name: 'Муры', url: 'assets/images/мури 1.png' },
   ];
 }
