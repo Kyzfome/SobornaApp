c1cc3eaa69b0abef4d5937c6e069da0cc3573fc1
Kyzfome2 <domka_201811@gmail.com>
2024-10-12 12:47:11 +0300

    change-institution-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { HistoryComponent } from './history/history.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 import { ImageSliderComponent } from './image-slider/image-slider.component';
@@ -19,7 +18,6 @@ import { InstitutionComponent } from './institution/institution.component';
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    HistoryComponent,
     ImageSliderComponent,
     ContainerComponent,
     PhotoComponent,
